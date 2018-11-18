a4248a963a0e4613b85e1536fc6dd25ffe972f15
Yasser Al-Hasan <yasseral-hasan@Yassers-MacBook-Pro.local>
2018-11-18 16:04:44 -0500

    modification to check process CPU usage rather than general CPU Usage

--- app/src/main/java/ca/concordia/project/projectathena/MainActivity.java
+++ app/src/main/java/ca/concordia/project/projectathena/MainActivity.java
@@ -13,7 +13,8 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textViewcpuUsage;
+//    private TextView textViewcpuUsage;
+    private TextView textViewProcessUsage;
     private Button buttonTurnOnProtection;
 
     @Override
@@ -22,40 +23,67 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //TextView
-        textViewcpuUsage = (TextView)findViewById(R.id.textViewcpuUsage);
-        textViewcpuUsage.setMovementMethod(new ScrollingMovementMethod());
+//        textViewcpuUsage = (TextView) findViewById(R.id.textViewcpuUsage);
+//        textViewcpuUsage.setMovementMethod(new ScrollingMovementMethod());
+
+        textViewProcessUsage = (TextView) findViewById(R.id.textViewProcessUsage);
+        textViewProcessUsage.setMovementMethod(new ScrollingMovementMethod());
 
         //Buttom
-        buttonTurnOnProtection = (Button)findViewById(R.id.buttonTurnOnProtection);
+        buttonTurnOnProtection = (Button) findViewById(R.id.buttonTurnOnProtection);
         buttonTurnOnProtection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 buttonTurnOnProtection.setText("IT IS ON!!!!!!!!!");
-                textViewcpuUsage.setText(checkCpuUsage());
+//                textViewcpuUsage.setText(checkCpuUsage());
+                textViewProcessUsage.setText(getProcessUsage());
             }
         });
     }
 
-    private static String checkCpuUsage(){
-        ProcessBuilder processBuilder;
-        String holder = "";
-        String[] DATA = {"/system/bin/cat", "/proc/cpuinfo"};
+
+//    private static String checkCpuUsage() {
+//        ProcessBuilder processBuilder;
+//        String holder = "";
+//        String[] DATA = {"/system/bin/cat", "/proc/cpuinfo"};
+//        InputStream inputStream;
+//        Process process;
+//        byte[] byteArry;
+//        byteArry = new byte[1024];
+//        try {
+//            processBuilder = new ProcessBuilder(DATA);
+//            process = processBuilder.start();
+//            inputStream = process.getInputStream();
+//            while (inputStream.read(byteArry) != -1) {
+//                holder = holder + new String(byteArry);
+//            }
+//            inputStream.close();
+//
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//        return holder;
+//    }
+
+    private static String getProcessUsage() {
+
+        String line = "";
+        String cmd = "top -m 1 -d 1 -n 1";
         InputStream inputStream;
-        Process process ;
-        byte[] byteArry ;
-        byteArry = new byte[1024];
-        try{
-            processBuilder = new ProcessBuilder(DATA);
-            process = processBuilder.start();
+        Process process;
+        byte[] byteArray;
+        byteArray = new byte[1024];
+        try {
+            process = Runtime.getRuntime().exec(cmd);
             inputStream = process.getInputStream();
-            while(inputStream.read(byteArry) != -1){
-                holder = holder + new String(byteArry);
+            while (inputStream.read(byteArray) != -1) {
+                line = line + new String(byteArray) + "\n";
             }
             inputStream.close();
 
-        } catch(IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
-        return holder;
+        return line;
     }
-    }
+}
